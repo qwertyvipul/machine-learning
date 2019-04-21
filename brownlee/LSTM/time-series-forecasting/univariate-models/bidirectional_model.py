2081c496753993d38681a51c2311b4873efb9713
Vipul Sharma <vsharma4_be16@thapar.edu>
2019-04-22 00:11:43 +0530

    brownlee LSTM models

---
+++ brownlee/LSTM/time-series-forecasting/univariate-models/bidirectional_model.py
@@ -0,0 +1,53 @@
+import numpy as np
+from keras.models import Sequential
+from keras.layers import LSTM
+from keras.layers import Dense
+from keras.layers import Bidirectional
+
+# Split a univariate sequence into samples
+def split_sequence(sequence, n_steps):
+    X, y = list(), list()
+    for i in range(len(sequence)):
+        # find the end of the pattern
+        end_ix = i + n_steps
+        if(end_ix > len(sequence) - 1):
+            break
+        
+        # gather the input and output parts of the pattern
+        seq_x, seq_y = sequence[i:end_ix], sequence[end_ix]
+        X.append(seq_x)
+        y.append(seq_y)
+    return np.array(X), np.array(y)
+
+# define input sequence
+raw_seq = [10, 21, 32, 43, 54, 65, 76, 87, 98]
+new_seq = list()
+for i in range(1, 100):
+    new_seq.append(i*20)
+
+# choose a number of timestamps
+n_steps = 3
+
+# Split into samples
+X, y = split_sequence(new_seq, n_steps)
+
+# reshape from [samples, timestamps] into [samples, timestamp, features]
+n_features = 1
+X = X.reshape((X.shape[0], X.shape[1], n_features))
+
+# define model
+model = Sequential()
+model.add(Bidirectional(LSTM(50, activation="relu"), input_shape=(n_steps, n_features)))
+model.add(Dense(1))
+model.compile(optimizer = "adam", loss = "mse")
+
+#fit model
+model.fit(X, y, batch_size = 10, epochs=100, verbose=0)
+
+# demostrate prediction
+X_input = np.array([60, 80, 100]) #103
+X_input = X_input.reshape((1, n_steps, n_features))
+yhat = model.predict(X_input, verbose = 0)
+
+print(yhat)
+# 104.14432
---
+++ brownlee/LSTM/time-series-forecasting/univariate-models/stacked_model.py
@@ -0,0 +1,51 @@
+import numpy as np
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import LSTM
+from keras.layers import Dense
+
+# Split a univariate sequence into samples
+def split_sequence(sequence, n_steps):
+    X, y = list(), list()
+    for i in range(len(sequence)):
+        # find the end of the pattern
+        end_ix = i + n_steps
+        if(end_ix > len(sequence) - 1):
+            break
+        
+        # gather the input and output parts of the pattern
+        seq_x, seq_y = sequence[i:end_ix], sequence[end_ix]
+        X.append(seq_x)
+        y.append(seq_y)
+    return np.array(X), np.array(y)
+
+# define input sequence
+raw_seq = [10, 21, 32, 43, 54, 65, 76, 87, 98]
+
+# choose a number of timestamps
+n_steps = 3
+
+# Split into samples
+X, y = split_sequence(raw_seq, n_steps)
+
+# reshape from [samples, timestamps] into [samples, timestamp, features]
+n_features = 1
+X = X.reshape((X.shape[0], X.shape[1], n_features))
+
+# define model
+model = Sequential()
+model.add(LSTM(50, activation="relu", return_sequences = True, input_shape=(n_steps, n_features)))
+model.add(LSTM(50, activation="relu"))
+model.add(Dense(1))
+model.compile(optimizer = "adam", loss = "mse")
+
+#fit model
+model.fit(X, y, epochs=100, verbose=0)
+
+# demostrate prediction
+X_input = np.array([70, 81, 92])
+X_input = X_input.reshape((1, n_steps, n_features))
+yhat = model.predict(X_input, verbose = 0)
+
+print(yhat)
+# 105.563446
